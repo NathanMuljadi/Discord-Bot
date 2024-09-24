ffaff3fee63bc032265a325da98280c23013b048
Nathan Muljadi <nathanjmuljadi@gmail.com>
2024-09-23 17:55:41 -0700

    Fixed bugs

--- src/commands.js
+++ src/commands.js
@@ -1,41 +1,49 @@
-import dotenv from "dotenv";
-import { REST, Routes, SlashCommandBuilder } from "discord.js";
+/**
+ * Register new slash commands
+ */
+import dotenv from 'dotenv';
+import { REST, Routes, SlashCommandBuilder } from 'discord.js';
+
+dotenv.config();
 
 /** Define commands */
 const commands = [
     new SlashCommandBuilder()
-        .setName("ama")
-        .setDescription("What's up? Ask me anything!")
-        .addStringOption(option => option.setName("question")
-                                         .setDescription("Body Text")
+        .setName('hello')
+        .setDescription('Replies with hello!'),
+    new SlashCommandBuilder()
+        .setName('embed')
+        .setDescription('Returns an embed')
+        .addStringOption(option => option.setName('text')
+                                         .setDescription('Body Text')
                                          .setRequired(true)),
     new SlashCommandBuilder()
-        .setName("genImg")
-        .setDescription("Generate an image given a prompt!")
-        .addStringOption(option => option.setName("text")
-                                        .setDescription("Body Text")
-                                        .setRequired(true)),
-]
+        .setName('ama')
+        .setDescription('Ask me anything :)')
+        .addStringOption(option => option.setName('text')
+                                         .setDescription('Body Text')
+                                         .setRequired(true)),
+];
 
-/** Register the commands */
+/** Register commands */
 const rest = new REST().setToken(process.env.DISCORD_API_KEY);
 
-/** Actually going to register the commands */
 (async () => {
     try {
-        // Registering slash commands
-        console.log("Registering slash commands...")
+        // Register commands here
+        console.log('Registering slash commands...');
 
         await rest.put(
             Routes.applicationGuildCommands(
-                process.env.CLIENT_ID,
-                process.env.SERVER_ID,
+                process.env.CLIENT_ID, 
+                process.env.SERVER_ID
             ),
-            { body : commands }
+            { body: commands }
         );
 
-        console.log("Commands registered succesfully!");
-    } catch (e) {
-        console.log(`There was an error: ${e}`);
+        console.log('Slash commands registered successfully!');
+    } catch (error) {
+        // Error handling
+        console.log(`There was an error: ${error}`);
     }
 })();
--- src/index.js
+++ src/index.js
@@ -1,68 +1,112 @@
-import OpenAI from "openai";
-import dotenv from "dotenv";
-import { Client, IntentsBitField, EmbedBuilder } from "discord.js";
+import OpenAI from 'openai';
+import dotenv from 'dotenv';
+import { Client, IntentsBitField, EmbedBuilder } from 'discord.js';
 
 /** Initialize dotenv so we can access environment variables */
 dotenv.config();
 
-/** Set up client object */
+/** Set bitflags for intents */
 const client = new Client({
     intents: [
-        IntentsBitField.Flags.Guilds, 
-        IntentsBitField.Flags.GuildMembers, 
-        IntentsBitField.Flags.GuildMessages, 
-        IntentsBitField.Flags.MessageContent
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
     ]
 })
 
-/** Create openai object */
+/** Create OpenAI Object */
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
+});
+
+/** Logging when discord bot is online and ready */
+client.on('ready', (c) => {
+    console.log(`${c.user.tag} is now running...`);
 })
 
-/** Test that bot is successfully online */
-client.on("ready", (c) => {
-    console.log(`${c.user.tag} is ready :D`);
+/** Bot responds to hello messages */
+client.on('messageCreate', (message) => {
+    /** Prevents bot from infinitely replying to itself */
+    if (message.author.bot) {
+        return
+    }
+
+    console.log(`${message.author.username}: ${message.content}`);
+
+    if (message.content.includes('hello')) {
+        message.reply(`Hello ${message.author.username}!`);
+    }
 })
 
-/** Function for generating text response */
+/** Function for text generation with OpenAI API */
 async function generateMessage(textPrompt) {
     const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
+        model: 'gpt-3.5-turbo',
         messages: [
             {
-                role: "system",
-                content: "You are a student answering your friend's question",
+                role: 'system',
+                content: 'You are a college student answering your classmate.',
             },
             {
-                role: "user",
+                role: 'user',
                 content: textPrompt,
-            }
-        ]
+            },
+        ],
     });
     return response.choices[0].message;
 }
 
-/** Add functionality -- We handle slash commands */
-client.on("interactionCreate", async (interaction) => {
+/** Function for image generation with OpenAI API */
+async function generateImage(imagePrompt) {
+    const response = await openai.images.generate({
+        model: 'dall-e-3',
+        prompt: imagePrompt,
+        size: "1024x1024",
+        quality: "standard",
+        n: 1,
+    })
+    console.log(response);
+    return response.data[0].url;
+}
+
+/** Handle slash commands */
+client.on('interactionCreate', async (interaction) => {
     if (!interaction.isChatInputCommand()) return;
     if (interaction.channel.id !== process.env.CHANNEL_ID) return;
 
-    if (interaction.commandName === "ama") {
-        /** We are thinking of a response */
+    if (interaction.commandName === 'embed') {
         await interaction.deferReply();
-        const prompt = interaction.options.get("text").value();
+        try {
+            const prompt = interaction.options.get('text').value;
+            const image = await generateImage(prompt);
+            /** Chain methods to define shape of embed */
+            const text = interaction.options.get('text').value;
+            const embed = new EmbedBuilder().setTitle(`${interaction.user.username}`)
+                                            .setDescription(`${prompt}`)
+                                            .setImage(image)
+                                            .setColor('Random');
+            await interaction.editReply({ embeds: [embed] });
+        } catch (e) {
+            console.log("Error occurred: ", e);
+            return await interaction.editReply('I\'m fresh out of images...');
+        }
+    }    
+
+    if (interaction.commandName === 'ama') {
+        /** Send typing status */
+        await interaction.deferReply();
+        const prompt = interaction.options.get('text').value;
 
         try {
-            //Get response
-            response = await generateMessage(prompt);
+            const response = await generateMessage(prompt);
             await interaction.editReply(response);
         } catch (e) {
-            console.log(`Error occurred: ${e}`);
-            return await interaction.editReply("IDK");
+            console.log("Error occurred: ", e);
+            return await interaction.editReply('You\'ve stumped me D:');
         }
     }
 })
 
-/** Start running bot instance */
+/** client is our bot instance, so we can call our client, bot should be online now */
 client.login(process.env.DISCORD_API_KEY);
